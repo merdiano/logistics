fb5caf2280b9745dbee184d3fb480633b236630b
merdiano <merdan.m@gmail.com>
2019-10-28 01:09:34 +0500

    get bid detail fix

--- api/app/Http/Controllers/ApplicationController.php
+++ api/app/Http/Controllers/ApplicationController.php
@@ -27,12 +27,12 @@ class ApplicationController extends Controller
     }
 
     public function get($app_id){
-        return $app_id;
+//        return $app_id;
         return BaseApplication::withCount('bids')
             ->with('pickup_location')
             ->with('destination_location')
             ->with('account')
-            ->with('winner')
+//            ->with('winner')
             ->findOrFail($app_id);
     }
 
